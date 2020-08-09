43cc6bdfa5e271d48cc3522de193d13c35ed8bfd
Gerard <ghager93@gmail.com>
2020-08-09 23:10:23 +1000

    pointsAreSubsetOfFunction

--- LoopSegment.py
+++ LoopSegment.py
@@ -8,6 +8,9 @@ class LoopSegment:
         self.bottomRight = bottomRight
         self.points = points
 
+    def entryAndExits(self):
+        return self.mergeNeighbouringPoints(self.edgePoints())
+
     def edgePoints(self):
         return [p for p in self.points if self.onEdge(p)]
 
@@ -15,18 +18,74 @@ class LoopSegment:
         return point.x == self.topLeft.x or point.y == self.topLeft.y or \
                point.x == self.bottomRight.x or point.y == self.bottomRight.y
 
+    @staticmethod
+    def mergeNeighbouringPoints(points: List[Vector2D]):
+        points = set(points)
+        mergedPoints = set()
+        visited = set()
+        for point in points:
+            if point not in visited:
+                neighbourhood = set()
+                stack = list()
+                stack.append(point)
+                while stack:
+                    point = stack.pop()
+                    visited.add(point)
+                    neighbourhood.add(point)
+                    for neighbour in points-visited:
+                        if point.manhattanDistanceTo(neighbour) == 1:
+                            stack.append(neighbour)
+                mergedPoints.add(min(neighbourhood))
+
+        return mergedPoints
+
     def width(self):
-        return self.bottomRight.x - self.topLeft.x
+        return self.bottomRight.x - self.topLeft.x + 1
 
     def height(self):
-        return self.bottomRight.y - self.topLeft.y
+        return self.bottomRight.y - self.topLeft.y + 1
 
     def mid(self):
         return Vector2D((self.topLeft.x + self.bottomRight.x)//2, (self.topLeft.y + self.bottomRight.y)//2)
 
     def asMatrix(self):
-        matrix = np.full((self.width()+1, self.height()+1), False)
+        matrix = np.full((self.width(), self.height()), False)
         for point in self.points:
             matrix[point.x-self.topLeft.x, point.y-self.topLeft.y] = True
 
         return matrix
+
+    # def pointsAreSubSetOfFunction(self):
+    #     return len(self.points) == len(set(p.x for p in self.points)) or \
+    #            len(self.points) == len(set(p.y for p in self.points))
+
+    def pointsAreSubsetOfFunction(self):
+        return self.horizontalLinesAreConsecutive() or self.verticalLinesAreConsecutive()
+
+    def horizontalLinesAreConsecutive(self):
+        points = sorted(self.points, key=lambda p: p.y)
+        currFirst = points[0]
+        currEnum = 0
+
+        for e, p in enumerate(points):
+            if p.y != currFirst.y:
+                currFirst = p
+                currEnum = e
+            elif p.x - currFirst.x != e - currEnum:
+                return False
+
+        return True
+
+    def verticalLinesAreConsecutive(self):
+        points = sorted(self.points, key=lambda p: p.x)
+        currFirst = points[0]
+        currEnum = 0
+
+        for e, p in enumerate(points):
+            if p.x != currFirst.x:
+                currFirst = p
+                currEnum = e
+            elif p.y - currFirst.y != e - currEnum:
+                return False
+
+        return True
--- tests/test_LoopSegment.py
+++ tests/test_LoopSegment.py
@@ -23,9 +23,31 @@ class TestLoopSegment(TestCase):
 
     def test_as_matrix(self):
         np.testing.assert_array_equal(self.segment.asMatrix(),
-                                             np.array([[True,  True,  False, False, False, False],
-                                                       [True,  True,  False, False, False, False],
-                                                       [False, False, False, False, False,  True],
-                                                       [False, False, False, False, False, False],
-                                                       [False, False, False, True,  False, False],
-                                                       [False, True,  False, False, False, False]]))
+                                      np.array([[True, True, False, False, False, False],
+                                                [True, True, False, False, False, False],
+                                                [False, False, False, False, False, True],
+                                                [False, False, False, False, False, False],
+                                                [False, False, False, True, False, False],
+                                                [False, True, False, False, False, False]]))
+
+    def test_merge_neighbouring_points(self):
+        pointList = [Vector2D(0, 0), Vector2D(1, 0), Vector2D(6, 0), Vector2D(7, 0), Vector2D(0, 1), Vector2D(9, 5),
+                     Vector2D(9, 4), Vector2D(9, 6), Vector2D(9, 9)]
+        assert set(LoopSegment.LoopSegment.mergeNeighbouringPoints(pointList)) == \
+               {Vector2D(0, 0), Vector2D(6, 0), Vector2D(9, 4), Vector2D(9, 9)}
+
+    def test_horizontal_lines_are_consecutive_true(self):
+        pointList = [Vector2D(0, 0), Vector2D(1, 0), Vector2D(1, 2), Vector2D(2, 3), Vector2D(3, 3)]
+        assert LoopSegment.LoopSegment(None, None, pointList).horizontalLinesAreConsecutive()
+
+    def test_horizontal_lines_are_consecutive_false(self):
+        pointList = [Vector2D(0, 0), Vector2D(1, 0), Vector2D(1, 2), Vector2D(2, 3), Vector2D(3, 3), Vector2D(5, 0)]
+        assert not LoopSegment.LoopSegment(None, None, pointList).horizontalLinesAreConsecutive()
+
+    def test_vertical_lines_are_consecutive_true(self):
+        pointList = [Vector2D(0, 0), Vector2D(1, 0), Vector2D(1, 2), Vector2D(2, 3), Vector2D(3, 3), Vector2D(1, 1)]
+        assert LoopSegment.LoopSegment(None, None, pointList).horizontalLinesAreConsecutive()
+
+    def test_vertical_lines_are_consecutive_false(self):
+        pointList = [Vector2D(0, 0), Vector2D(1, 0), Vector2D(1, 2), Vector2D(2, 3), Vector2D(3, 3), Vector2D(5, 0)]
+        assert not LoopSegment.LoopSegment(None, None, pointList).horizontalLinesAreConsecutive()
