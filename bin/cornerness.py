a0b9af035449cc2977a97ba9405999baf04a343f
ghage <ghager93@gmail.com>
2020-11-24 16:54:17 +1100

    cornerness.py _cornerness

--- bin/cornerness.py
+++ bin/cornerness.py
@@ -3,7 +3,14 @@ import numpy as np
 from bin.image_array import pad_by_zeroes
 
 
-def _get_shift_neighbour_arrays(array: np.ndarray):
+def _cornerness(array: np.ndarray):
+    out = 8-sum(_get_shifted_neighbour_arrays(array))
+    out[array == 0] = 0
+
+    return out
+
+
+def _get_shifted_neighbour_arrays(array: np.ndarray):
     padded_array = pad_by_zeroes(array)
     return [padded_array[x:padded_array.shape[0] + x-2, y:padded_array.shape[1] + y-2]
             for x in range(3) for y in range(3)
