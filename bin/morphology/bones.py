aa10e4b5bbbed9034006adbfecdbb6813580a6da
ghage <ghager93@gmail.com>
2020-12-15 14:48:15 +1100

    JointGraph

--- bin/morphology/bones.py
+++ bin/morphology/bones.py
@@ -15,11 +15,11 @@ class Bone(tuple):
     def __hash__(self):
         hash_ = 0
         if len(self) > 0:
-            hash_ += super.__hash__(self[0])
+            hash_ += super.__hash__(tuple([self[0]]))
         if len(self) > 1:
-            hash_ += super.__hash__(self[-1])
+            hash_ += super.__hash__(tuple([self[-1]]))
         if len(self) > 2:
-            hash_ += super.__hash__(self[1:-1])
+            hash_ += super.__hash__(tuple([self[1:-1]]))
 
         return hash_
 
@@ -30,6 +30,22 @@ class Bone(tuple):
         return self[-1]
 
 
+class JointGraph(dict):
+    def __init__(self, joints, bones):
+        super().__init__()
+        self.joints = joints
+        self.bones = bones
+        self._build_graph()
+
+    def _build_graph(self):
+        for joint in self.joints:
+            self[joint] = set()
+
+        for bone in self.bones:
+            self[bone.start()].add(bone.end())
+            self[bone.end()].add(bone.start())
+
+
 def bones(skeleton_graph: SkeletonGraph):
     visited = set()
     bones = set()
@@ -37,13 +53,13 @@ def bones(skeleton_graph: SkeletonGraph):
     for joint in skeleton_graph.joints():
         unvisited_neighbours = {joint + n for n in neighbour_coordinates(skeleton_graph.edges[joint])} - visited
         for node in unvisited_neighbours:
-            bone = Bone([joint])
+            bone = [joint]
             while node not in skeleton_graph.joints():
                 bone.append(node)
                 visited.add(node)
                 node = ({node + n for n in neighbour_coordinates(skeleton_graph.edges[node])}
                         - (visited | {joint})).pop()
             bone.append(node)
-            bones.add(bone)
+            bones.add(Bone(bone))
 
     return bones
