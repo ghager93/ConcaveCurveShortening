4c7cd08aaf15428ba2799397328e3bb828f6f3b4
ghage <ghager93@gmail.com>
2020-12-15 15:00:29 +1100

    added JointGraph to SkeletonGraph

--- bin/morphology/skeleton_graph.py
+++ bin/morphology/skeleton_graph.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from . import skeleton_ops
 from . import transforms
+from . import bones
 
 
 class SkeletonGraph:
@@ -13,6 +14,7 @@ class SkeletonGraph:
         self.ends = skeleton_ops.end_points(self.skeleton)
         self.branches = skeleton_ops.branch_points(self.skeleton)
         self.root = skeleton_ops.image_root(self.image, self.skeleton)
+        self.joint_graph = bones.JointGraph(self.joints(), bones.bones(self))
 
     def joints(self):
         return self.ends | self.branches | {self.root}
